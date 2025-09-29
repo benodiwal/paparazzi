68cc70fd160458eef85eb1c7661592fa809df7b3
benodiwal <s474996633@gmail.com>
2025-09-29 20:08:31 +0530

    fix: removed depricated versions usage

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,46 @@
 use global_hotkey::{hotkey::{Code, HotKey, Modifiers}, GlobalHotKeyEvent, GlobalHotKeyManager};
-use winit::event_loop::{ControlFlow, EventLoopBuilder};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::application::ApplicationHandler;
 use anyhow::Result;
 
 mod screenshot;
 
-fn main() -> Result<()> {
-    let event_loop = EventLoopBuilder::new().build()?;
-    let manager = GlobalHotKeyManager::new()?;
+struct App {
+    receiver: crossbeam_channel::Receiver<GlobalHotKeyEvent>,
+}
 
-    let hotkey = HotKey::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyS);
-    manager.register(hotkey)?;
-
-    let receiver = GlobalHotKeyEvent::receiver();
-    event_loop.run(move |_event, elwt| {
-        elwt.set_control_flow(ControlFlow::Wait);
-
-        if let Ok(event) = receiver.try_recv() {
+impl ApplicationHandler for App {
+    fn resumed(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {}
+    
+    fn window_event(
+        &mut self,
+        _event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        _event: winit::event::WindowEvent,
+    ) {
+    }
+    
+    fn new_events(&mut self, event_loop: &winit::event_loop::ActiveEventLoop, _cause: winit::event::StartCause) {
+        event_loop.set_control_flow(ControlFlow::Wait);
+        
+        if let Ok(event) = self.receiver.try_recv() {
             if event.state == global_hotkey::HotKeyState::Pressed {
                 println!("Hotkey pressed! Taking screenshot...");
             }
         }
-    })?;
+    }
+}
 
+fn main() -> Result<()> {
+    let event_loop = EventLoop::new()?;
+    let manager = GlobalHotKeyManager::new()?;
+    let hotkey = HotKey::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyS);
+    manager.register(hotkey)?;
+    let receiver = GlobalHotKeyEvent::receiver().to_owned();
+    
+    let mut app = App { receiver };
+    
+    event_loop.run_app(&mut app)?;
+    
     Ok(())
 }
